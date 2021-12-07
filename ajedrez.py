2e35bcee1dc1220069b74b8ed9c87e15511dbcfc
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2021-12-07 18:40:50 +0100

    commit

--- ajedrez.py
+++ ajedrez.py
@@ -1,36 +1,44 @@
 import tablero_fichero
+from tablero_fichero import f
 from tablero_listas import tablero
-tablero_fichero.tablero_inicial()
+a = 0
+f.write("Movimiento " + str(a))
+tablero_fichero.tablero_fichero()
 while True:
     continuar = input("¿Quieres realizar un movimiento? ")
-    if continuar == "SI":
+    if continuar == "si":
+        a = a + 1
         while True:
-            inicio = input("Elija la fila y la columna de la pieza que desea mover separadas por espacios: ")
+            inicio = input("Elija la fila y la columna de la pieza que desea mover, separadas por espacios: ")
             inicio = inicio.split()
-            filaI = inicio[0]
-            columnaI = inicio[1]
-            try:
-                filaI = int(filaI)
-                columnaI = int(columnaI)
-            except:
-                pass
-            else:
-                if filaI >= 0 and filaI < 8 and columnaI >= 0 and columnaI < 8:
-                    break
+            if len(inicio) == 2:
+                filaI = inicio[0]
+                columnaI = inicio[1]
+                try:
+                    filaI = int(filaI)
+                    columnaI = int(columnaI)
+                except:
+                    pass
+                else:
+                    if filaI >= 0 and filaI < 8 and columnaI >= 0 and columnaI < 8:
+                        break
         while True:
-            final = input("Elija la fila y la columna de la pieza que desea mover separadas por espacios: ")
+            final = input("Elija la fila y la columna a la que desea mover la pieza, separadas por espacios: ")
             final = final.split()
-            filaF = final[0]
-            columnaF = final[1]
-            try:
-                filaF = int(final)
-                columnaF = int(columnaF)
-            except:
-                pass
-            else:
-                if filaF >= 0 and filaF < 8 and columnaF >= 0 and columnaF < 8:
-                    tablero[filaF][columnaF] = tablero[filaI]
-                    tablero[filaI][columnaI] = " "
-                    break
-    else:
+            if len(final) == 2:
+                filaF = final[0]
+                columnaF = final[1]
+                try:
+                    filaF = int(filaF)
+                    columnaF = int(columnaF)
+                except:
+                    pass
+                else:
+                    if filaF >= 0 and filaF < 8 and columnaF >= 0 and columnaF < 8 and final != inicio:
+                        tablero[filaF][columnaF] = tablero[filaI][columnaI]
+                        tablero[filaI][columnaI] = " "
+                        break
+        f.write("Movimiento " + str(a))
+        tablero_fichero.tablero_fichero()
+    elif continuar == "no":
         break
--- tablero_listas.py
+++ tablero_listas.py
@@ -1,5 +1,19 @@
-from tablero_fichero import blancas, negras
-
+blancas = {
+    'torre': chr(0x2656),
+    'caballo': chr(0x2658),
+    'alfil': chr(0x2657),
+    'reina': chr(0x2655),
+    'rey': chr(0x2654),
+    'peon': chr(0x2659)
+} 
+negras = {
+    'torre': chr(0x265C),
+    'caballo': chr(0x265E),
+    'alfil': chr(0x265D),
+    'reina': chr(0x265B),
+    'rey': chr(0x265A),
+    'peon': chr(0x265F)
+}
 tablero = []
 for i in range(8):
     tablero.append([' '] * 8)
