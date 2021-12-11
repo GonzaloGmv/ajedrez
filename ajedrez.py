05a6635ba93b52f97b8d5ba0ee01398543da48b5
GonzaloGmv <91721237+GonzaloGmv@users.noreply.github.com>
2021-12-11 19:07:27 +0100

    commit

--- ajedrez.py
+++ ajedrez.py
@@ -22,6 +22,7 @@ while True:
                     filaI = int(filaI)
                     columnaI = int(columnaI)
                 except:
+                    print("No son válidas")
                     pass
                 else:
                     if filaI >= 0 and filaI < 8 and columnaI >= 0 and columnaI < 8:
@@ -29,6 +30,10 @@ while True:
                             break
                         elif movimiento % 2 == 0 and piezas_negras.__contains__(tablero[filaI][columnaI]):
                             break
+                        else:
+                            print("No son válidas")
+                    else:
+                        print("No son válidas")
         while True:
             final = input("Elija la fila y la columna a la que desea mover la pieza, separadas por espacios: ")
             final = final.split()
@@ -39,16 +44,21 @@ while True:
                     filaF = int(filaF)
                     columnaF = int(columnaF)
                 except:
+                    print("No son válidas")
                     pass
                 else:
                     if filaF >= 0 and filaF < 8 and columnaF >= 0 and columnaF < 8 and final != inicio:
                         tablero[filaF][columnaF] = tablero[filaI][columnaI]
                         tablero[filaI][columnaI] = " "
                         break
+                    else:
+                        print("No son válidas")
         f.write("Movimiento " + str(movimiento))
         tablero_fichero.tablero_fichero()
     elif continuar == "no":
         break
+    else:
+        print("Escriba si o no")
 while True:
     mostrar_mov = input("¿Desea que se muestre en pantalla algún movimiento? ")
     if mostrar_mov == "si":
@@ -57,11 +67,16 @@ while True:
             try:
                 paso = int(paso)
             except:
+                print("Escriba un numero para referirse al movimiento que quiere que se le muestre")
                 pass
             else:
                 if paso <= movimiento and paso >= 0:
                     break
+                else:
+                    print("Ese movimiento no existe")
         tablero_fichero.leer_tablero(paso)
     elif mostrar_mov == "no":
         f.close
         break
+    else:
+        print("Escriba si o no")
